c174e4619ede2fb89e3b5df1a1e629f376a2b629
camil2000 <mariacamillapra18@gmail.com>
2025-04-03 09:04:44 -0500

    Commit pepe

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from datetime import datetime, date, time
 from typing import ClassVar
 
+#Hola
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
 
